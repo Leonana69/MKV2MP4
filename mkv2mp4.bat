23af319dea580476b8ab39b1406ac1aff685b7f1
leonana69 <dikong.cgj@alibaba-inc.com>
2019-07-16 22:04:14 +0800

    return to h264 since hevc format can't be correctly recognized by ae

--- mkv2mp4.bat
+++ mkv2mp4.bat
@@ -4,7 +4,7 @@ CD/D "%~dp0"
 :loop
 IF "%~1"=="" GOTO :STOP
 
-ffmpeg -i "%~1" -vcodec copy -acodec aac "%~dpn1.mp4"
+ffmpeg -i "%~1" -vcodec copy -acodec aac -b:a 320k "%~dpn1.mp4"
 shift/1
 goto :loop
 
